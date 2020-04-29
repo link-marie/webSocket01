662cc50d6d81e683ad3af53db71d3a59f13392b7
link-marie <rijkmarie@gmail.com>
2020-04-29 18:31:12 +0900

    clean up codes for better understanding

--- server.js
+++ server.js
@@ -6,19 +6,19 @@ const { Server } = require('ws');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express()
+const serverHttp = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const wss = new Server({ server });
+const serverWs = new Server({ server: serverHttp });
 
-wss.on('connection', (ws) => {
+serverWs.on('connection', (socket) => {
   console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+  socket.on('close', () => console.log('Client disconnected'));
 });
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
+  serverWs.clients.forEach((client) => {
     client.send(new Date().toTimeString());
   });
 }, 1000);
